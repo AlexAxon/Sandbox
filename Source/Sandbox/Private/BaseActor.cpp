d83f7943e9ee838bc41605667749d17d71f7fd32
Алексей Аксёнов <aksyonov.a@theinvaders.pro>
2026-03-13 21:18:26 +0300

    Lesson 2

--- Source/Sandbox/Private/BaseActor.cpp
+++ Source/Sandbox/Private/BaseActor.cpp
@@ -1,6 +1,3 @@
-// ITHub 2026
-
-
 #include "BaseActor.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogMain, Error, Error);
@@ -8,37 +5,101 @@ DEFINE_LOG_CATEGORY_STATIC(LogMain, Error, Error);
 ABaseActor::ABaseActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(MeshComp);
 }
 
 void ABaseActor::BeginPlay()
 {
 	Super::BeginPlay();
-	PrintTypes();
+	StartLocation = GetActorLocation();
+	PrintTransform();
+	
+	if (EnableColor)
+	{
+		if (UMaterialInstanceDynamic* DynamicMaterial = MeshComp->CreateAndSetMaterialInstanceDynamic(0))
+		{
+			DynamicMaterial->SetVectorParameterValue("BaseColor", StartColor);
+		}
+	}
 	
 }
 
-void ABaseActor::Tick(float DeltaTime)
+void ABaseActor::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	AddActorLocalOffset(FVector(0, 0, 1));
+	Movement();
 }
 
-void ABaseActor::PrintTypes()
+void ABaseActor::PrintStats() const
 {
-	FString KillsStr = "Kills: " + FString::FromInt(Kills);
-	FString HealthStr = "Health: " + FString::SanitizeFloat(Health);
-	FString IsDeadStr = "Is dead " + FString(IsDead ? "true" : "false");
+	const FString KillsStr = "Kills: " + FString::FromInt(Kills);
+	const FString HealthStr = "Health: " + FString::SanitizeFloat(Health);
+	const FString IsDeadStr = "Is dead " + FString(IsDead ? "true" : "false");
 
-	FString Stat = FString::Printf(TEXT("\n \n +++ Stat +++ \n %s \n %s \n %s \n %s "), 
-									*Name, *KillsStr, *HealthStr, *IsDeadStr);
+	const FString Stats = FString::Printf(TEXT("\n \n +++ Stat +++ \n %s \n %s \n %s \n %s "), *Name, *KillsStr, *HealthStr, *IsDeadStr);
 
-	UE_LOG(LogMain, Error, TEXT("%s"), *Stat);
 	UE_LOG(LogMain, Error, TEXT("   Actor name: %s"), *GetName());
+	UE_LOG(LogMain, Error, TEXT("%s"), *Stats);
+	
 	//UE_LOG(LogMain, Error, TEXT("Kills: %i"), Kills);
 	//UE_LOG(LogMain, Error, TEXT("Health: %.2f"), Health);
 	//UE_LOG(LogMain, Error, TEXT("IsDead: %d"), IsDead);
 	//UE_LOG(LogMain, Error, TEXT("IsDead: %d"), static_cast<int>(IsDead));
 
-	GEngine->AddOnScreenDebugMessage(-1, 20.0f, FColor::Red, Stat);
+	GEngine->AddOnScreenDebugMessage(-1, 20.0f, FColor::Red, Stats);
 }
 
+void ABaseActor::PrintTransform() const
+{
+	const FTransform Transform = GetActorTransform();
+	const FVector Location = Transform.GetLocation();
+	const FRotator Rotator = Transform.Rotator();
+	const FVector Scale = Transform.GetScale3D();
+	
+	UE_LOG(LogMain, Error, TEXT("Actor name %s"), *GetName());
+	UE_LOG(LogMain, Error, TEXT("Transform %s"), *Transform.ToString());
+	UE_LOG(LogMain, Error, TEXT("Location %s"), *Location.ToString());
+	UE_LOG(LogMain, Error, TEXT("Rotator %s"), *Rotator.ToString());
+	UE_LOG(LogMain, Error, TEXT("Scale %s"), *Scale.ToString());
+	UE_LOG(LogMain, Error, TEXT("Readable transform %s"), *Transform.ToHumanReadableString());
+}
+
+void ABaseActor::Movement()
+{
+	float Time = GetWorld()->GetTimeSeconds();
+
+	switch (MovementSettings.MovementType)
+	{
+	case EMovementType::Static:
+		{
+		}
+		break;
+	case EMovementType::SinVertical:
+		{
+			FVector CurrentLocation = GetActorLocation();
+			CurrentLocation.Z = StartLocation.Z + MovementSettings.Amplitude * FMath::Sin(MovementSettings.Frequency * Time);
+			SetActorLocation(CurrentLocation);
+		}
+		break;
+	case EMovementType::SinHorizontalX:
+		{
+			FVector CurrentLocation = GetActorLocation();
+			CurrentLocation.X = StartLocation.X + MovementSettings.Amplitude * FMath::Sin(MovementSettings.Frequency * Time);
+			SetActorLocation(CurrentLocation);
+		}
+		break;
+	case EMovementType::SinHorizontalY:
+		{
+			FVector CurrentLocation = GetActorLocation();
+			CurrentLocation.Y = StartLocation.Y + MovementSettings.Amplitude * FMath::Sin(MovementSettings.Frequency * Time);
+			SetActorLocation(CurrentLocation);
+		}
+		break;
+	case EMovementType::Up:
+		{
+			AddActorLocalOffset(FVector::UnitZ());
+		}
+		break;
+	}
+}
--- Source/Sandbox/Public/BaseActor.h
+++ Source/Sandbox/Public/BaseActor.h
@@ -1,10 +1,34 @@
-// ITHub 2026
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 #include "BaseActor.generated.h"
+
+UENUM(BlueprintType)
+enum class EMovementType : uint8
+{
+	SinVertical,
+	SinHorizontalX,
+	SinHorizontalY,
+	Static,
+	Up
+};
+
+USTRUCT(BlueprintType)
+struct FMovementSettings
+{
+	GENERATED_USTRUCT_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	EMovementType MovementType = EMovementType::Static;
+	
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Amplitude = 50.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Frequency = 2.0f;
+};
 
 UCLASS()
 class SANDBOX_API ABaseActor : public AActor
@@ -21,9 +45,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	void PrintTypes();
+	void PrintStats() const;
+	
+	void PrintTransform() const;
+	
+	void Movement();
 
 public:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+	
 	UPROPERTY(VisibleAnywhere, Category = "Stats")
 	int32 Kills = 5;
 
@@ -35,5 +66,19 @@ public:
 
 	UPROPERTY(EditInstanceOnly, Category = "Stats")
 	FString Name = "Alex";
-
+	
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	FMovementSettings MovementSettings;
+	
+	UPROPERTY(EditAnywhere, Category = "Color")
+	bool EnableColor = false;
+	
+	UPROPERTY(EditAnywhere, Category = "Color")
+	FLinearColor StartColor;
+	
+	UPROPERTY(EditAnywhere, Category = "Color")
+	float ColorOffset = 1.0f;
+	
+private:
+	FVector StartLocation;
 };
